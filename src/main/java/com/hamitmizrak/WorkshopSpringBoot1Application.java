f3c5243fc1ba60c4a9becd82c7b773376229b50c
Hamit Mizrak <hamitmizrak@outlook.com>
2024-08-20 10:08:42 +0300

    Pom.xml

--- src/main/java/com/hamitmizrak/WorkshopSpringBoot1Application.java
+++ src/main/java/com/hamitmizrak/WorkshopSpringBoot1Application.java
@@ -3,11 +3,56 @@ package com.hamitmizrak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+//@EntityScan(basePackages = "com.hamitmizrak.data.entity") //Entity bulamadığı zaman
+//@EnableJpaRepositories(basePackages = "com.hamitmizrak.data.repository") //Repository bulamadığı zaman
+//@ComponentScan("com")
+
+// Spring Cache aktif etmek gerekiyor.
+// @EnableCaching
+
+// Auditing Aktif etmek
+//@EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme, çünkü Bcrypted kullancağım ancak Spring security için gerekli kütüphaneleri dahil
+// Buradaki exclude ne zaman kapatmam gerekiyor ? cevap: Spring Security ile çalıştığımız zaman kapat
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+
+// Apache Tomcat için: extends SpringBootServletInitializer
+//@SpringBootApplication
 public class WorkshopSpringBoot1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkshopSpringBoot1Application.class, args);
-	}
+    public static void main(String[] args) {
 
-}
+        // devtools active isActive
+        // System.setProperty("spring.devtools.restart.enabled","true");
+
+        // PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+         */
+
+        // JOptional pane aktif etmek
+        System.setProperty("java.awt.headless", "false");
+
+        // Main
+        SpringApplication.run(WorkshopSpringBoot1Application.class, args);
+    } // end PSVM
+
+} // end class

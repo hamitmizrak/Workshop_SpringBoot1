e1d0bd3e70aff1019bc911914e339b3e7778e0fc
Hamit Mizrak <hamitmizrak@outlook.com>
2024-08-20 10:05:21 +0300

    Initial commit

---
+++ src/test/java/com/hamitmizrak/WorkshopSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
